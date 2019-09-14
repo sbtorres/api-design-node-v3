afd689ab3394e0763caeef8fd053ea35e34c8013
Scott Torres <scott.torres27@gmail.com>
2019-09-13 17:24:50 -0700

    Implement routes for item

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,17 @@
 import { Router } from 'express'
+import { getHome, postHome, getId, deleteId, putId } from './item.controllers'
 
 const router = Router()
+
+router
+  .route('/')
+  .get(getHome)
+  .post(postHome)
+
+router
+  .route('/:id')
+  .get(getId)
+  .delete(deleteId)
+  .put(putId)
 
 export default router

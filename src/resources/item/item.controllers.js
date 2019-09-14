14914ca9934001c53e5d30c1012944fd53ab7ed2
Scott Torres <scott.torres27@gmail.com>
2019-09-13 17:24:37 -0700

    Implement controllers for the different item routes

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,19 @@
-export default {}
+export const getHome = (req, res) => {
+  res.status(200).send({ message: 'this is the home get route' })
+}
+
+export const postHome = (req, res) => {
+  res.status(201).send({ message: 'this is the home post route' })
+}
+
+export const getId = (req, res) => {
+  res.status(200).send({ message: 'this is the id get route' })
+}
+
+export const deleteId = (req, res) => {
+  res.status(204).send({ message: 'this is the id delete route' })
+}
+
+export const putId = (req, res) => {
+  res.status(204).send({ message: 'this is the id post route' })
+}

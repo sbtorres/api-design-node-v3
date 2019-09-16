9118c53c56ae8e8a5b0c1d5480675e97d1040a6e
Scott Torres <scott.torres27@gmail.com>
2019-09-16 16:44:29 -0700

    Implement model and controllers

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import controllers from './item.controllers'
+import Item from './item.model'
 
 const router = Router()
 
 // /api/item
 router
   .route('/')
-  .get(controllers.getOne)
+  .get(controllers.getMany)
   .post(controllers.createOne)
 
 // /api/item/:id
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,57 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+  const document = await model.findOne({ _id: id, createdBy: userId }).exec()
+  if (document) {
+    res.status(200).json({ data: document })
+  } else {
+    res.status(404).end()
+  }
+}
 
-export const getMany = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  let docs = await model.find({ createdBy: req.user._id }).exec()
+  if (docs) {
+    res.status(200).json({ data: docs })
+  } else {
+    res.status(404).json({ message: 'could not find any items' })
+  }
+}
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  const doc = await model.create({ ...req.body, createdBy: req.user._id })
+  res.status(201).json({ data: doc })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  const doc = await model.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      createdBy: req.user._id
+    },
+    req.body,
+    { new: true }
+  )
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const removeOne = model => async (req, res) => {
+  const doc = await model
+    .findOneAndRemove({
+      _id: req.params.id,
+      createdBy: req.user._id
+    })
+    .exec()
+
+  if (!doc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
